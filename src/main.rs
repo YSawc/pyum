0c0ac6a9c5166679fc21f9741164105437236860
YSawc <niceage6690@gmail.com>
2024-07-28 09:55:52 +0900

    update: add routes handles devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,116 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{Html, IntoResponse, Json},
+    routing::{get, post},
+    Router,
+};
+use diesel::prelude::*;
+use diesel::SelectableHelper;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use pyum::{
+    models::{Device, NewDevice},
+    schema::devices,
+};
+use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+// this embeds the migrations into the application binary
+// the migration path is relative to the `CARGO_MANIFEST_DIR`
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "tokio_mysql=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+
+    // set up connection pool
+    let manager = deadpool_diesel::mysql::Manager::new(db_url, deadpool_diesel::Runtime::Tokio1);
+    let pool = deadpool_diesel::mysql::Pool::builder(manager)
+        .build()
+        .unwrap();
+
+    // run the migrations on server startup
+    {
+        let conn = pool.get().await.unwrap();
+        conn.interact(|conn| conn.run_pending_migrations(MIGRATIONS).map(|_| ()))
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    let app = Router::new()
+        .route("/api/health_check", get(health_check_handler))
+        .route("/device/list", get(list_devices))
+        .with_state(pool);
+
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+pub async fn health_check_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "API Services";
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
+async fn list_devices(
+    State(pool): State<deadpool_diesel::mysql::Pool>,
+) -> Result<Json<Vec<Device>>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let res = conn
+        .interact(|conn| devices::table.select(Device::as_select()).load(conn))
+        .await
+        .map_err(internal_error)?
+        .map_err(internal_error)?;
+    Ok(Json(res))
+}
+
+async fn create_device(
+    State(pool): State<deadpool_diesel::mysql::Pool>,
+    Json(new_devise): Json<NewDevice>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    // ) -> Result<Json<Device>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let res = conn
+        .interact(|conn| {
+            diesel::insert_into(devices::table)
+                .values(new_devise)
+                .execute(conn)
+                .unwrap();
+            // .get_result(conn)
+        })
+        .await
+        .map_err(internal_error)?;
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": "insert successed."
+    });
+
+    Ok(Json(json_response))
+}
+
+// Utility function for mapping any error into a `500 Internal Server Error`
+// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,17 @@
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::devices)]
+use crate::schema;
+
+#[derive(serde::Serialize, Queryable, Selectable)]
+#[diesel(table_name = schema::devices)]
 #[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Device {
     pub id: i32,
     pub name: String,
+}
+
+#[derive(serde::Deserialize, Insertable)]
+#[diesel(table_name = schema::devices)]
+pub struct NewDevice {
+    name: String,
 }

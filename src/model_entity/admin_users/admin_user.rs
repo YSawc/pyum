9bd608d986f0d9d3b5ac0a2f713804bf6a3a1e3c
YSawc <niceage6690@gmail.com>
2024-08-03 18:07:17 +0900

    update: add admin_user schema and migrations.

---
+++ src/model_entity/admin_users/admin_user.rs
@@ -0,0 +1,20 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "admin_users")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub name: String,
+    #[serde(skip_serializing)]
+    pub encrypted_password: String,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
--- src/model_entity/mod.rs
+++ src/model_entity/mod.rs
@@ -1,1 +1,2 @@
+pub mod admin_users;
 pub mod devices;

97919d6eb41528d8358b286e95db17262a615c36
YSawc <niceage6690@gmail.com>
2024-08-11 08:04:38 +0900

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -81,10 +81,6 @@ async fn main() -> anyhow::Result<()> {
                 // logging of errors so disable that
                 .on_failure(()),
         )
-        // .layer(axum::middleware::from_fn_with_state(
-        //     state.clone(),
-        //     handle_error,
-        // ))
         .with_state(state);
 
     // run it with hyper
@@ -266,12 +262,3 @@ async fn post_edit_device(
 
     Ok(Redirect::to("/device/"))
 }
-
-// // Utility function for mapping any error into a `500 Internal Server Error`
-// // response.
-// fn internal_error<E>(err: E) -> (StatusCode, String)
-// where
-//     E: std::error::Error,
-// {
-//     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-// }

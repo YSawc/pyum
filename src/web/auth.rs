ca812adaffa3b9be0e0d929b6b315d23e61fff25
YSawc <niceage6690@gmail.com>
2024-08-15 15:13:29 +0900

    update: add request test for not exists route.

--- src/web/auth.rs
+++ src/web/auth.rs
@@ -21,4 +21,17 @@ mod tests {
         println!("res: {:?}", res);
         assert_eq!(res.status(), StatusCode::OK);
     }
+
+    #[tokio::test]
+    async fn request_nonprepared_route() {
+        let db_url = std::env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL is not set");
+        env::set_var("DATABASE_URL", db_url);
+        let req = Request::builder()
+            .uri("/not_exists_route")
+            .body(Body::empty())
+            .unwrap();
+        let res = routes::router().await.oneshot(req).await.unwrap();
+        println!("res: {:?}", res);
+        assert_eq!(res.status(), StatusCode::NOT_FOUND);
+    }
 }

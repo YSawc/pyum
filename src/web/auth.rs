c5a61a0bbe5190f8bff75a10774bb751fa8f4419
YSawc <niceage6690@gmail.com>
2024-08-15 21:28:27 +0900

    modify: add header checking for uid and cookie_id.

--- src/web/auth.rs
+++ src/web/auth.rs
@@ -1,3 +1,36 @@
+use axum::{
+    body::Body,
+    extract::Request,
+    http::{Response, StatusCode},
+    middleware::Next,
+    response::IntoResponse,
+};
+
+use crate::web::middleware::buffer_and_print;
+
+pub async fn check_session_id(
+    req: Request,
+    next: Next,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let (parts, body) = req.into_parts();
+    let maybe_uid = parts.headers.get("uid");
+    let maybe_cookie_id = parts.headers.get("cookie_id");
+    if maybe_uid.is_none() || maybe_cookie_id.is_none() {
+        return Err((
+            StatusCode::EXPECTATION_FAILED,
+            "uid or cookie_id is not set.".to_string(),
+        ));
+    }
+    let bytes = buffer_and_print("request", body).await?;
+    let req = Request::from_parts(parts, Body::from(bytes));
+    let res = next.run(req).await;
+    let (parts, body) = res.into_parts();
+    let bytes = buffer_and_print("response", body).await?;
+    let res = Response::from_parts(parts, Body::from(bytes));
+
+    Ok(res)
+}
+
 #[cfg(test)]
 mod tests {
     use axum::{
@@ -18,7 +51,6 @@ mod tests {
             .body(Body::empty())
             .unwrap();
         let res = routes::router().await.oneshot(req).await.unwrap();
-        println!("res: {:?}", res);
         assert_eq!(res.status(), StatusCode::OK);
     }
 
@@ -31,20 +63,46 @@ mod tests {
             .body(Body::empty())
             .unwrap();
         let res = routes::router().await.oneshot(req).await.unwrap();
-        println!("res: {:?}", res);
         assert_eq!(res.status(), StatusCode::NOT_FOUND);
     }
 
     #[tokio::test]
-    async fn request_for_protected_route() {
+    async fn missing_header_uid_for_protected_route() {
         let db_url = std::env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL is not set");
         env::set_var("DATABASE_URL", db_url);
         let req = Request::builder()
             .uri("/device/")
+            .header("cookie_id", "Bar")
             .body(Body::empty())
             .unwrap();
         let res = routes::router().await.oneshot(req).await.unwrap();
-        println!("res: {:?}", res);
+        assert_eq!(res.status(), StatusCode::EXPECTATION_FAILED);
+    }
+
+    #[tokio::test]
+    async fn missing_header_cookie_id_for_protected_route() {
+        let db_url = std::env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL is not set");
+        env::set_var("DATABASE_URL", db_url);
+        let req = Request::builder()
+            .uri("/device/")
+            .header("uid", "Bar")
+            .body(Body::empty())
+            .unwrap();
+        let res = routes::router().await.oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::EXPECTATION_FAILED);
+    }
+
+    #[tokio::test]
+    async fn meet_header_for_protected_route() {
+        let db_url = std::env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL is not set");
+        env::set_var("DATABASE_URL", db_url);
+        let req = Request::builder()
+            .uri("/device/")
+            .header("uid", "Bar")
+            .header("cookie_id", "Bar")
+            .body(Body::empty())
+            .unwrap();
+        let res = routes::router().await.oneshot(req).await.unwrap();
         assert_eq!(res.status(), StatusCode::OK);
     }
 }
--- src/web/routes/protected.rs
+++ src/web/routes/protected.rs
@@ -1,17 +1,10 @@
 use axum::{
-    body::Body,
-    extract::Request,
-    http::StatusCode,
-    middleware::{self, Next},
-    response::IntoResponse,
+    middleware,
     routing::{get, post},
     Router,
 };
 
-use crate::web::{
-    controllers::*,
-    middleware::{buffer_and_print, AppState},
-};
+use crate::web::{auth::check_session_id, controllers::*, middleware::AppState};
 
 pub fn router() -> Router<AppState> {
     Router::new()
@@ -23,16 +16,4 @@ pub fn router() -> Router<AppState> {
         .route("/device/:device_id/edit", post(device::post_edit_device))
         .route("/device/:device_id/delete", get(device::delete_device))
         .layer(middleware::from_fn(check_session_id))
-}
-
-pub async fn check_session_id(
-    req: Request,
-    next: Next,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let (parts, body) = req.into_parts();
-    println!("{:?}", parts.headers);
-    let bytes = buffer_and_print("request", body).await?;
-    let req = Request::from_parts(parts, Body::from(bytes));
-    let res = next.run(req).await;
-    Ok(res)
 }

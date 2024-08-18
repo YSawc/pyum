57baedda4c77f189454483a05844065568a29f18
YSawc <niceage6690@gmail.com>
2024-08-18 14:34:50 +0900

    modify: add header cookie for response of post login api

--- src/web/controllers/admin_user.rs
+++ src/web/controllers/admin_user.rs
@@ -1,9 +1,9 @@
 use axum::extract::State;
 
-use axum::http::StatusCode;
-use axum::response::{Html, Redirect};
+use axum::http::{HeaderMap, HeaderValue, StatusCode};
+use axum::response::{Html, IntoResponse, Redirect};
 use axum::Form;
-use model_entity::models::admin_user;
+use model_entity::models::{admin_user, session};
 
 use crate::web::middleware::AppState;
 
@@ -45,10 +45,33 @@ pub async fn get_login_admin_user(
 pub async fn post_login_admin_user(
     state: State<AppState>,
     Form(admin_user): Form<admin_user::model::Model>,
-) -> Result<Redirect, (StatusCode, &'static str)> {
-    admin_user::mutation::find_by_name(&state.conn, admin_user)
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    if let Some(admin_user) = admin_user::mutation::find_by_name(&state.conn, admin_user)
         .await
-        .unwrap();
-
-    Ok(Redirect::to("/device/"))
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "failed to finding admin user by name.",
+            )
+        })
+        .unwrap()
+    {
+        session::mutation::seed_unexpired_with_admin_user_id(&state.conn, admin_user.id)
+            .await
+            .expect("failed to create session");
+        let all_session =
+            session::mutation::find_unexpired_by_admin_user_id(&state.conn, admin_user.id)
+                .await
+                .expect("");
+        let first_session = all_session.first().unwrap();
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "cookie",
+            HeaderValue::from_str(format!("cid={:?}", first_session.cookie_id.clone()).as_str())
+                .unwrap(),
+        );
+        Ok((headers, Redirect::to("/device/")).into_response())
+    } else {
+        Ok(Redirect::to("/admin_user/login").into_response())
+    }
 }

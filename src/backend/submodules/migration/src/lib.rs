d012dd6eea87133c06ffaf8bed9e5e5355341fd7
YSawc <niceage6690@gmail.com>
2024-08-25 16:03:21 +0900

    chore: rename device migration

--- src/backend/submodules/migration/src/lib.rs
+++ src/backend/submodules/migration/src/lib.rs
@@ -1,7 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20240801_000001_create_device_table;
 mod m20240803_063030_create_admin_user_table;
+mod m20240803_064001_create_device_table;
 mod m20240803_090516_add_created_at_and_updated_at_column_to_device;
 mod m20240803_221344_create_oauth2_client_secret;
 mod m20240814_125552_create_session;
@@ -12,8 +12,8 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20240801_000001_create_device_table::Migration),
             Box::new(m20240803_063030_create_admin_user_table::Migration),
+            Box::new(m20240803_064001_create_device_table::Migration),
             Box::new(m20240803_090516_add_created_at_and_updated_at_column_to_device::Migration),
             Box::new(m20240803_221344_create_oauth2_client_secret::Migration),
             Box::new(m20240814_125552_create_session::Migration),

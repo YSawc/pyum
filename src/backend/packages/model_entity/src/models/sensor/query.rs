a3e55abc1be91c73e92e79ae11e3bb9371dd3cbc
YSawc <niceage6690@gmail.com>
2024-09-13 08:25:33 +0900

    modify: modify query function for device with nested relation.

--- src/backend/packages/model_entity/src/models/sensor/query.rs
+++ src/backend/packages/model_entity/src/models/sensor/query.rs
@@ -1,6 +1,5 @@
-use crate::models::sensor_purpose;
-
 use super::{model, model::Entity as Sensor};
+use crate::models::{device, sensor_purpose};
 use sea_orm::*;
 
 pub struct SensorQuery;
@@ -24,5 +23,43 @@ impl SensorQuery {
             .iter()
             .map(|elm| (elm.0.to_owned(), elm.1.first().unwrap().to_owned()))
             .collect::<Vec<_>>())
+    }
+
+    pub async fn find_devices_with_related_sensor_and_purpose(
+        db: &DbConn,
+        page: u64,
+        models_per_page: u64,
+    ) -> Result<
+        Vec<(
+            device::model::Model,
+            Vec<(super::model::Model, sensor_purpose::model::Model)>,
+        )>,
+        DbErr,
+    > {
+        let devices_with_sensor = device::model::Entity::find()
+            .find_with_related(super::model::Entity)
+            .limit(models_per_page)
+            .offset(models_per_page * (page - 1))
+            .all(db)
+            .await
+            .unwrap();
+        let mut devices_and_related_sensors_and_purposes = Vec::new();
+        for device_with_sensor in devices_with_sensor {
+            let sensors = device_with_sensor.1;
+            let mut sensors_and_purposes = Vec::new();
+            for sensor in sensors {
+                let purpose = sensor
+                    .find_related(sensor_purpose::model::Entity)
+                    .one(db)
+                    .await
+                    .unwrap()
+                    .unwrap();
+                sensors_and_purposes.push((sensor, purpose));
+            }
+            devices_and_related_sensors_and_purposes
+                .push((device_with_sensor.0, sensors_and_purposes))
+        }
+
+        Ok(devices_and_related_sensors_and_purposes)
     }
 }
